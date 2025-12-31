28a3684beb7cc2b314e8cc01f709fbc15d64437f
Sandeep K <sandeep@priyarajaelectronics.com>
2025-12-31 14:40:41 +0530

    Setup Header layout with logo,name,profile icon

--- ems/next.config.ts
+++ ems/next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+       domains: ["i.pravatar.cc"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.jdmagicbox.com",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
